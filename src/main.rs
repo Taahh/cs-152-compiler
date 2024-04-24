848cf83acc26c57b996e389cf322f785496b5bcc
Taah <admin@taah.dev>
2024-04-24 16:10:45 -0700

    fix tokenizing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
 use std::{env, fs};
+use std::num::ParseIntError;
 
 mod interpreter;
+
+#[derive(Debug, Clone)]
+enum Token {
+    NotToken,
+    Plus,
+    Subtract,
+    Multiply,
+    Divide,
+    Modulus,
+    Assign,
+
+    Comma,
+    SemiColon,
+
+    Num(i32),
+    Ident(String),
+    If,
+    Else,
+    Break,
+    While,
+
+    Func,
+    Return,
+
+    Int,
+
+    OpenParentheses,
+    ClosingParentheses,
+    OpeningBrace,
+    ClosingBrace,
+    OpeningBracket,
+    ClosingBracket,
+
+    Print,
+    Read,
+
+    GreaterThan,
+    LessThan,
+    GreaterThanEqual,
+    LessThanEqual,
+
+    Equality,
+    NotEqual
+}
 
 fn main() {
     // get commandline arguments.
@@ -34,51 +79,131 @@ fn main() {
     let mut word_buffer = String::new();
     let mut is_comment = true & false | false;
     let mut tokens: Vec<String> = vec![];
+    let mut token_types: Vec<Token> = vec![];
 
-    for c in code.chars(){
-
+    for c in code.chars() {
         buffer.push(c);
         if c == '#' // dump buffer
         {
-            is_comment = true;          
+            is_comment = true;
         }
-        if c == '\n' 
+        if c == '\n'
         {
             // comment identification
-            if is_comment == true  
+            if is_comment == true
             {
                 print!("Filtered comment: ");
                 print!("{}", buffer);
                 is_comment = false;
             } else {
-                    for ch in buffer.chars() 
+                for ch in buffer.chars()
+                {
+                    if ch == ' ' || ch == '(' || ch == ')' || ch == '}' || ch == '{' || ch == '[' || ch == ']' || ch == ';'
                     {
-                        if ch == ' ' || ch == '(' || ch == ')' || ch == '}' || ch == '{' || ch == ';'
+                        if (word_buffer.len() > 0) {
+                            word_buffer = word_buffer.to_lowercase();
+                            token_types.push(parse(word_buffer.clone()));
+
+                            tokens.push(word_buffer.clone());
+                        }
+
+                        if (ch != ' ') {
+                            token_types.push(parse(ch.to_string().clone()));
+                        }
+                        word_buffer.clear();
+                    } else {
+                        if ch != '\n'
                         {
-                            // println!("{}",word_buffer);
-                            if word_buffer.len() > 0
-                            {  
-                                tokens.push(word_buffer.clone());
-                            }
-                            if ch != ' ' && ch != '\n'
-                            {
-                                // println!("{}", ch);
-                                tokens.push(ch.clone().to_string())
-                            }
-                            word_buffer.clear();
-                        } else {
-                            if ch != '\n'
-                            {
-                                word_buffer.push(ch);
-                            }
+                            // let token = parse(ch.clone().to_string());
+                            // if token != Token::NotToken {
+                            //     token_types.push(token);
+                            // }
+                            word_buffer.push(ch);
                         }
                     }
-                
-                    
+                }
             };
             buffer.clear();
         }
     }
+
+    fn parse(s: String) -> Token {
+        let mut arr: Vec<Token> = vec![];
+        if (s.eq("+")) {
+            return Token::Plus;
+        } else if (s.eq("-")) {
+            return Token::Subtract;
+        } else if (s.eq("*")) {
+            return Token::Multiply;
+        } else if (s.eq("/")) {
+            return Token::Divide;
+        } else if (s.eq("%")) {
+            return Token::Modulus;
+        } else if (s.eq("=")) {
+            return Token::Assign;
+        } else if (s.eq("if")) {
+            return Token::If;
+        }  else if (s.eq("else")) {
+            return Token::Else;
+        } else if (s.eq("while")) {
+            return Token::While;
+        }  else if (s.eq("break")) {
+            return Token::Break;
+        } else if (s.eq("read")) {
+            return Token::Read;
+        } else if (s.eq("func")) {
+            return Token::Func;
+        } else if (s.eq("return")) {
+            return Token::Return;
+        } else if (s.eq("int")) {
+            return Token::Int;
+        } else if (s.eq("(")) {
+            return Token::OpenParentheses;
+        }  else if (s.eq(")")) {
+            return Token::ClosingParentheses;
+        }  else if (s.eq("{")) {
+            return Token::OpeningBrace;
+        }  else if (s.eq("}")) {
+            return Token::ClosingBrace;
+        }  else if (s.eq("[")) {
+            return Token::OpeningBracket;
+        }  else if (s.eq("]")) {
+            return Token::ClosingBracket;
+        }  else if (s.eq(",")) {
+            return Token::Comma;
+        }   else if (s.eq(";")) {
+            return Token::SemiColon;
+        }  else if (s.eq("==")) {
+            return Token::Equality;
+        }  else if (s.eq("!=")) {
+            return Token::NotEqual;
+        } else if (s.eq("print")) {
+            return Token::Print;
+        }  else if (s.eq(">")) {
+            return Token::GreaterThan;
+        }  else if (s.eq("<")) {
+            return Token::LessThan;
+        }  else if (s.eq(">=")) {
+            return Token::GreaterThanEqual;
+        }  else if (s.eq("<=")) {
+            return Token::LessThanEqual;
+        }
+        else {
+            let test_if_num = s.parse::<i32>();
+            match test_if_num {
+                Ok(num) => {
+                    return Token::Num(num);
+                }
+                Err(_) => {
+                    println!("String: {}", s);
+                    return Token::Ident(s);
+                }
+            }
+        }
+
+        return Token::NotToken;
+    }
     // output
     println!("{:?}", tokens);
+    println!("{:?}", token_types);
 }
